38593747b5ceed6d876c48417690ee3dde01763d
agung <ag.setiawan@id.jmtech.co.jp>
2015-12-11 17:14:27 +0900

    id-rails coming soon

--- js/common.js
+++
@@ -1,8 +0,0 @@
-var leftHigh=document.getElementById("sidebar").clientHeight;
-var rightHigh=document.getElementById("main-content").clientHeight;
-var higher=Math.max(leftHigh, rightHigh);
-
-document.getElementById("sidebar").setAttribute("style","height:"+higher+"px");
-document.getElementById("main-content").setAttribute("style","height:"+higher+"px");
-
-
